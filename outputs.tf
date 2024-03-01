1c230719c032f07b2fec8439c6971d3863c7a93c
simran <simran.r@iqzsystems.com>
2024-03-01 20:35:17 +0530

    output instance ip

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_name" {
   value       = aws_instance.ec2_instance.id
   description = "The id of the created ec2 instance."
 }
+
+output "instance_ip" {
+  value       = aws_eip.instance_ip.address
+  description = "The IP of the created ec2 instance."
+}
