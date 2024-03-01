4d9b3b5cea995077c74db86e2b04d9a101b9c4bd
simran <simran.r@iqzsystems.com>
2024-03-01 20:38:06 +0530

    output instance ip

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "instance_name" {
 }
 
 output "instance_ip" {
-  value       = aws_eip.instance_ip.address
+  value       = aws_instance.ec2_instance.public_ip
   description = "The IP of the created ec2 instance."
 }
